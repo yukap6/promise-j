92c0416f467531ba8d2bd2e5641cfc48954b1f80
weirong.jing <wb-jwr216045@alibaba-inc.com>
2017-11-17 09:59:00 +0800

    then chained use is ok

--- test/index.js
+++ test/index.js
@@ -3,13 +3,17 @@ const Promise = require('../index');
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('hello promise');
-    // resolve('fulfilled');
-    reject('rejected');
-  }, 1000);
+    resolve('fulfilled');
+    // reject('rejected');
+  }, 100);
 });
 
 p.then((data) => {
   console.log('success', data);
 }, (error) => {
   console.log('error', error);
+}).then((data) => {
+  console.log('success1', data);
+}, (error) => {
+  console.log('error1', error);
 });
